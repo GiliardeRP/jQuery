6e0bbf5d9d2ac3057666d677125f8d4e4624ef46
_King;-;Pereira_ <g1l14rd3@gmail.com>
2022-09-13 20:26:21 -0300

    first commit

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,21 @@
+var frase = $(".frase").text();
+
+var numPalavras = frase.split(" ").length;
+
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var conteudo = campo.val();
+
+    //Retira os espaço da String 
+    var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
+
+    var qtdCaracteres = conteudoSemEspaco.length;
+    $('#contador-caracteres').text(qtdCaracteres);
+})
